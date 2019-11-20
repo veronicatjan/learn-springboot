15f5dcd5ebfee51e496ac675f80bda663699e59d
zengwz <zengwz@sucsoft.com>
2019-11-20 16:14:23 +0800

    added swagger 2 depedency and enabled it . can see it by accessing localhost:8080/v2/api-docs

--- src/main/java/com/sucsoft/vero/courseapidata/CourseApiDataApplication.java
+++ src/main/java/com/sucsoft/vero/courseapidata/CourseApiDataApplication.java
@@ -2,8 +2,10 @@ package com.sucsoft.vero.courseapidata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CourseApiDataApplication {
 
     public static void main(String[] args) {
