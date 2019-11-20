34fca323f96bd8f15fbeb7a17941fec61bae109f
zengwz <zengwz@sucsoft.com>
2019-11-20 15:17:09 +0800

    Test

--- src/main/java/com/sucsoft/vero/courseapidata/Topic/Topic.java
+++ src/main/java/com/sucsoft/vero/courseapidata/Topic/Topic.java
@@ -1,7 +1,15 @@
 package com.sucsoft.vero.courseapidata.Topic;
 
-//This is the entity class
+//This is the class that we need to turn into entity
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
+
+    //For primary keys annotate with @Id
+    @Id
     private String id;
     private String name;
     private String description;
--- src/main/java/com/sucsoft/vero/courseapidata/Topic/TopicService.java
+++ src/main/java/com/sucsoft/vero/courseapidata/Topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.sucsoft.vero.courseapidata.Topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -7,39 +8,45 @@ import java.util.Arrays;
 import java.util.List;
 
 //This is a spring business service ~
+
 @Service
 public class TopicService {
 
-    private List<Topic> topics =
-            new ArrayList<>(Arrays.asList(
-                    new Topic("Spring", "Spring Framework", "Spring Framework Description"),
-                    new Topic("Java", "Java", "Java Description"),
-                    new Topic("Springboot", "Springboot Framework", "Springboot Framework Description")
-            ));
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getAllTopics() {
+        // return topics;
+
+        /* Since the findAll() is an Iterable,
+        * we need to convert it to list
+        * forEach(topics::add) will add for each of the element
+        * in interable will be added to the list topics*/
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll()
+                .forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+//        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        Topic topics;
+        return topicRepository.findById(id).orElse(null);
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
+    /*the save() can be used for both add and update method
+    * as the Topic object has id info which is enough
+    * for CrudRepository to update.
+    * if exist -> update, if not -> add */
     public void updateTopic(Topic topic, String id) {
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equals(id));
+        topicRepository.deleteById(id);
     }
 }
